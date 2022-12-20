d5a0ed1d9595a7e9fd1fa09ec5bb942706e41227
GitHub Action <action@github.com>
2022-12-20 23:10:30 +0000

    0.0.13
    release downgraded version

--- src/Collectors/PublicClassMethodCollector.php
+++ src/Collectors/PublicClassMethodCollector.php
@@ -19,13 +19,27 @@ use Twig\Extension\ExtensionInterface;
  */
 final class PublicClassMethodCollector implements Collector
 {
-    public function __construct(
-        private readonly ApiDocStmtAnalyzer $apiDocStmtAnalyzer,
-        private readonly PublicClassMethodMatcher $publicClassMethodMatcher,
-        private readonly Configuration $configuration,
-    ) {
+    /**
+     * @readonly
+     * @var \TomasVotruba\UnusedPublic\ApiDocStmtAnalyzer
+     */
+    private $apiDocStmtAnalyzer;
+    /**
+     * @readonly
+     * @var \TomasVotruba\UnusedPublic\PublicClassMethodMatcher
+     */
+    private $publicClassMethodMatcher;
+    /**
+     * @readonly
+     * @var \TomasVotruba\UnusedPublic\Configuration
+     */
+    private $configuration;
+    public function __construct(ApiDocStmtAnalyzer $apiDocStmtAnalyzer, PublicClassMethodMatcher $publicClassMethodMatcher, Configuration $configuration)
+    {
+        $this->apiDocStmtAnalyzer = $apiDocStmtAnalyzer;
+        $this->publicClassMethodMatcher = $publicClassMethodMatcher;
+        $this->configuration = $configuration;
     }
-
     public function getNodeType(): string
     {
         return ClassMethod::class;
@@ -43,7 +57,7 @@ final class PublicClassMethodCollector implements Collector
 
         // skip test methods
         $classMethodName = $node->name->toString();
-        if (str_starts_with($classMethodName, 'test')) {
+        if (strncmp($classMethodName, 'test', strlen('test')) === 0) {
             return null;
         }
 
