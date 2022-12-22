b79e766842d9547c7451d8e69ef760c29c12f99a
Tomas Votruba <tomas.vot@gmail.com>
2022-12-22 17:34:17 +0100

    skip calls in tests case (#9)

---
+++ tests/Rules/UnusedPublicClassMethodRule/Source/TestCaseUser.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicClassMethodRule\Source;
+
+use PHPUnit\Framework\TestCase;
+use TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicClassMethodRule\Fixture\UsedInTestCaseOnly;
+
+final class TestCaseUser extends TestCase
+{
+    private function go(UsedInTestCaseOnly $usedInTestCaseOnly)
+    {
+        $usedInTestCaseOnly->useMe();
+    }
+}
