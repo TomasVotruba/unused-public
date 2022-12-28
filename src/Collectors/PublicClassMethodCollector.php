0049072ba3c737ea5d79f283b338d06b52b263e0
Markus Staab <maggus.staab@googlemail.com>
2022-12-28 13:34:37 +0100

    Restore src/Collectors/PublicClassMethodCollector.php

--- src/Collectors/PublicClassMethodCollector.php
+++ src/Collectors/PublicClassMethodCollector.php
@@ -43,10 +43,8 @@ final class PublicClassMethodCollector implements Collector
      * @param ClassMethod $node
      * @return array<array{class-string, string, int}>|null
      */
-    public function processNode(
-        Node $node,
-        Scope $scope
-    ): ?array {
+    public function processNode(Node $node, Scope $scope): ?array
+    {
         if (! $this->configuration->shouldCollectMethods()) {
             return null;
         }
@@ -89,10 +87,7 @@ final class PublicClassMethodCollector implements Collector
         $methodName = $node->name->toString();
 
         // is this method required by parent contract? skip it
-        if ($this->publicClassMethodMatcher->isUsedByParentClassOrInterface(
-            $classReflection,
-            $methodName
-        )) {
+        if ($this->publicClassMethodMatcher->isUsedByParentClassOrInterface($classReflection, $methodName)) {
             return null;
         }
 
