bc0461f502009bea9eb9a43cf735fd07dadc250f
Markus Staab <m.staab@complex-it.de>
2023-04-13 13:44:34 +0200

    Cover enum methods in tests

---
+++ tests/Rules/UnusedPublicClassMethodRule/Source/SomeEnum.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicClassMethodRule\Source;
+
+enum SomeEnum
+{
+    public function unused()
+    {
+    }
+
+    static public function unusedStatic()
+    {
+    }
+
+    public function useMe()
+    {
+    }
+
+    static public function useMeStatic()
+    {
+    }
+
+}
+
+
