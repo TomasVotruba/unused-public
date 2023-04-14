69126e46cd6baf29d95fcd6b269760f436455d8a
Markus Staab <m.staab@complex-it.de>
2023-04-14 09:55:39 +0200

    fix

--- tests/Rules/UnusedPublicPropertyRule/Fixture/SkipInheritedPublicApiProperty.php
+++ tests/Rules/UnusedPublicPropertyRule/Fixture/SkipInheritedPublicApiProperty.php
@@ -4,15 +4,11 @@ declare(strict_types=1);
 
 namespace TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicClassConstRule\Fixture;
 
+use TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicPropertyRule\Source\BasePropertyClass;
+
 final class SkipInheritedApiPublicProperty extends BasePropertyClass
 {
     public $property = 'overridden!';
 }
 
 
-class BasePropertyClass {
-    /**
-     * @api
-     */
-    public $property = 'paths';
-}
---
+++ tests/Rules/UnusedPublicPropertyRule/Source/BasePropertyClass.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicPropertyRule\Source;
+
+class BasePropertyClass {
+    /**
+     * @api
+     */
+    public $property = 'paths';
+}
