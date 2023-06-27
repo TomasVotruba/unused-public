4814f0e3be07ab5e8d2c8854d279b4030a6294f9
Markus Staab <markus.staab@redaxo.de>
2023-06-27 11:06:07 +0200

    Skip ProtectedToPrivateFixer for tests/Rules/ProtectedClassMethodInFinalClassRule/*

--- ecs.php
+++ ecs.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use PhpCsFixer\Fixer\PhpUnit\PhpUnitTestAnnotationFixer;
+use PhpCsFixer\Fixer\ClassNotation\ProtectedToPrivateFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
@@ -10,7 +11,8 @@ return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
 
     $ecsConfig->skip([
-        PhpUnitTestAnnotationFixer::class => 'tests/Rules/UnusedPublicClassMethodRule/Fixture/SkipTestCaseAnnotationMethod.php'
+        PhpUnitTestAnnotationFixer::class => 'tests/Rules/UnusedPublicClassMethodRule/Fixture/SkipTestCaseAnnotationMethod.php',
+        ProtectedToPrivateFixer::class => 'tests/Rules/ProtectedClassMethodInFinalClassRule/*'
     ]);
 
     $ecsConfig->sets([
