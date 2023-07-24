813787eba45f3a372eb50ba63d19be5adee8a554
Tomas Votruba <tomas.vot@gmail.com>
2023-07-24 10:50:18 +0200

    make own test provider for tests only

---
+++ tests/Rules/UnusedPublicClassMethodRule/Fixture/Tests/SkipTestAnnotationMethod.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicClassMethodRule\Fixture\Tests;
+
+final class SkipTestAnnotationMethod
+{
+    /**
+     * @test
+     */
+    public function someMethodAnnotatedTest()
+    {
+    }
+}
--- tests/Rules/UnusedPublicClassMethodRule/Fixture/Tests/SkipTestCasePublicMethod.php
+++ tests/Rules/UnusedPublicClassMethodRule/Fixture/Tests/SkipTestCasePublicMethod.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicClassMethodRule\Fixture;
+namespace TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicClassMethodRule\Fixture\Tests;
 
 use PHPUnit\Framework\TestCase;
 
