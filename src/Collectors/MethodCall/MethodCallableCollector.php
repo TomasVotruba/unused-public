f7698171b1cd89d5bec827b4df96ae1b785cb5d9
GitHub Action <action@github.com>
2023-07-25 15:45:17 +0000

    release PHP 7.2 downgraded 0.2.0

--- src/Collectors/MethodCall/MethodCallableCollector.php
+++ src/Collectors/MethodCall/MethodCallableCollector.php
@@ -20,12 +20,40 @@ use TomasVotruba\UnusedPublic\Configuration;
  */
 final class MethodCallableCollector implements Collector
 {
+    /**
+     * @readonly
+     * @var \TomasVotruba\UnusedPublic\ClassMethodCallReferenceResolver
+     */
+    private $classMethodCallReferenceResolver;
+
+    /**
+     * @readonly
+     * @var \TomasVotruba\UnusedPublic\Configuration
+     */
+    private $configuration;
+
+    /**
+     * @readonly
+     * @var \TomasVotruba\UnusedPublic\ClassTypeDetector
+     */
+    private $classTypeDetector;
+
+    /**
+     * @readonly
+     * @var \TomasVotruba\UnusedPublic\CallReferece\CallReferencesFlatter
+     */
+    private $callReferencesFlatter;
+
     public function __construct(
-        private readonly ClassMethodCallReferenceResolver $classMethodCallReferenceResolver,
-        private readonly Configuration $configuration,
-        private readonly ClassTypeDetector $classTypeDetector,
-        private readonly CallReferencesFlatter $callReferencesFlatter,
+        ClassMethodCallReferenceResolver $classMethodCallReferenceResolver,
+        Configuration $configuration,
+        ClassTypeDetector $classTypeDetector,
+        CallReferencesFlatter $callReferencesFlatter
     ) {
+        $this->classMethodCallReferenceResolver = $classMethodCallReferenceResolver;
+        $this->configuration = $configuration;
+        $this->classTypeDetector = $classTypeDetector;
+        $this->callReferencesFlatter = $callReferencesFlatter;
     }
 
     public function getNodeType(): string
--- tests/Rules/UnusedPublicClassMethodRule/templates/blade/skip_caller_tag.blade.php
+++
@@ -1,7 +0,0 @@
-@php
-    /** @var $skipTag \TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicClassMethodRule\Fixture\Blade\SkipTag */
-@endphp
-
-@if ($skipTag->condition())
-    // show
-@endif
