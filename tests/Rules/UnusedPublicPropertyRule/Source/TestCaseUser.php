9c72bd6055770ec872cc0fdcf060cd108dd4cfb3
Markus Staab <markus.staab@redaxo.de>
2023-07-25 12:25:14 +0200

    simplify

--- tests/Rules/UnusedPublicPropertyRule/Source/TestCaseUser.php
+++ tests/Rules/UnusedPublicPropertyRule/Source/TestCaseUser.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicPropertyRule\Source;
 
 use PHPUnit\Framework\TestCase;
-use TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicClassMethodRule\Fixture\UsedInTestCaseOnly;
+use TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicPropertyRule\Fixture\UsedInTestCaseOnly;
 
 final class TestCaseUser extends TestCase
 {
     private function go()
     {
-        $o = new \TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicPropertyRule\Fixture\UsedInTestCaseOnly();
+        $o = new UsedInTestCaseOnly();
         $o->property = 'a value';
     }
 }
