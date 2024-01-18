2130d466846b453d4ec41d7b67a3fa36c5905e56
Yuta Nagamiya <y.nagamiya@gmail.com>
2024-01-18 14:28:12 +0900

    Fix bug
    When static and non-static properties are used together, the non-static
    property is falsely detected as unused.

---
+++ tests/Rules/UnusedPublicPropertyRule/Source/PublicStaticPropertyClass.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicPropertyRule\Source;
+
+class PublicStaticPropertyClass
+{
+    public $staticProperty = 'public static';
+}
