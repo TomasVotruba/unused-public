21592f0c8f6e6d8893155f8043828f65a32aa4b1
Philip Hofstetter <phofstetter@sensational.ch>
2024-06-13 09:06:15 +0200

    add phpstan error identifiers (#118)

--- src/Rules/UnusedPublicPropertyRule.php
+++ src/Rules/UnusedPublicPropertyRule.php
@@ -74,6 +74,7 @@ final readonly class UnusedPublicPropertyRule implements Rule
                         ->file($filePath)
                         ->line($line)
                         ->tip(RuleTips::SOLUTION_MESSAGE)
+                        ->identifier('public.property.unused')
                         ->build();
                 }
             }
