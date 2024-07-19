d50653f0c3766f1baacdb5bd21c6eb68339819a2
Markus Staab <markus.staab@redaxo.de>
2024-07-19 11:37:57 +0200

    Detect public property used via Subclass

---
+++ tests/Rules/UnusedPublicPropertyRule/Fixture/PropertyUsedViaSubClass.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicPropertyRule\Fixture;
+
+class UseBaseClassViaSubClass
+{
+    private SubClass $prop;
+
+    protected function doFoo() {
+        $this->prop->x = 1;
+    }
+}
+
+class SubClass extends BaseClass
+{
+}
+
+class BaseClass
+{
+    public int $x;
+}
