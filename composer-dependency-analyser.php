e87c43516639d2114dd8a2e8a78c670e7bb19f2a
Tomas Votruba <tomas.vot@gmail.com>
2025-03-07 16:26:18 +0100

    bump deps

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -5,7 +5,4 @@ declare(strict_types=1);
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
-$config = new Configuration();
-
-return $config
-    ->ignoreErrorsOnPaths([__DIR__ . '/tests'], [ErrorType::UNKNOWN_CLASS]);
+return (new Configuration())->ignoreErrorsOnPaths([__DIR__ . '/tests'], [ErrorType::UNKNOWN_CLASS]);
