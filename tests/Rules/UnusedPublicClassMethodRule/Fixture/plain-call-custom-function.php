5bb643a632e5a6d88f300dc6af114b191271cdff
Tomas Votruba <tomas.vot@gmail.com>
2025-11-14 11:48:31 +0100

    raise

--- tests/Rules/UnusedPublicClassMethodRule/Fixture/plain-call-custom-function.php
+++ tests/Rules/UnusedPublicClassMethodRule/Fixture/plain-call-custom-function.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicClassMethodRule\Fixture;
 
 /**
- * @var \TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicClassMethodRule\Source\Caller1 $caller1
+ * @var \TomasVotruba\UnusedPublic\Tests\Rules\UnusedPublicClassMethodRule\Source\Caller1
  */
 
 myFunc([$caller1, 'callIt']);
